625c991ce7b5940c9b8d8cb3bf335d9a582e537b
ljq <lidaxia2022@gmail.com>
2024-09-22 12:47:42 +0800

    新增异常规范

--- swak-lib/swak-client/src/main/java/com/swak/lib/client/entity/ApiResult.java
+++ swak-lib/swak-client/src/main/java/com/swak/lib/client/entity/ApiResult.java
@@ -1,8 +1,11 @@
 package com.swak.lib.client.entity;
 
+import com.swak.lib.client.exception.SysBizError;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+
+import java.util.Objects;
 
 /**
  * 接口通用返回报文
@@ -21,5 +24,34 @@ public class ApiResult<T> {
 
     @ApiModelProperty("data: 业务数据")
     private T data;
+
+    public boolean isSuccess() {
+        return SysBizError.SUCCESS.getCode().equals(code);
+    }
+
+    public boolean isSuccessNoData() {
+        return isSuccess() && Objects.isNull(data);
+    }
+
+    public static ApiResult<Void> success() {
+        return success(null);
+    }
+
+    public static <T> ApiResult<T> success(T data) {
+        ApiResult<T> apiResult = new ApiResult<>();
+        apiResult.setCode(SysBizError.SUCCESS.getCode());
+        apiResult.setMsg(SysBizError.SUCCESS.getMsg());
+        apiResult.setData(data);
+        return apiResult;
+    }
+
+    public static ApiResult<Void> sysError() {
+        ApiResult<Void> apiResult = new ApiResult<>();
+        apiResult.setCode(SysBizError.SYS_ERROR.getCode());
+        apiResult.setMsg(SysBizError.SYS_ERROR.getMsg());
+        return apiResult;
+    }
+
+
 
 }
---
+++ swak-lib/swak-client/src/main/java/com/swak/lib/client/exception/SwakError.java
@@ -0,0 +1,15 @@
+package com.swak.lib.client.exception;
+
+/**
+ * 异常接口
+ *
+ * @author: ljq
+ * @date: 2024/9/22
+ */
+public interface SwakError {
+
+    String getCode();
+
+    String getMsg();
+
+}
