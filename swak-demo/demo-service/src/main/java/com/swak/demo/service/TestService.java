7d24098012930c86a4de2a9b54cbd7cedc2f0c35
ljq <lidaxia2022@gmail.com>
2024-10-25 00:20:09 +0800

    统一jackson

---
+++ swak-demo/demo-service/src/main/java/com/swak/demo/service/TestService.java
@@ -0,0 +1,15 @@
+package com.swak.demo.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author: ljq
+ * @date: 2024/10/24
+ */
+@AllArgsConstructor
+@Service
+public class TestService {
+
+
+}
---
+++ swak-lib/swak-common/src/main/java/com/swak/lib/common/jackson/JacksonConfig.java
@@ -0,0 +1,21 @@
+package com.swak.lib.common.jackson;
+
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 统一jackson配置
+ * @author: ljq
+ * @date: 2024/10/25
+ */
+@Configuration
+public class JacksonConfig {
+
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        return JacksonTools.jackson2ObjectMapperBuilderCustomizer();
+    }
+
+}
