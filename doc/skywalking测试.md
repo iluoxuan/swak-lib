b14b42db9a50f3112a8d67b29379e4414c14b341
ljq <lidaxia2022@gmail.com>
2024-11-07 17:49:42 +0800

    添加  skywalking

---
+++ doc/skywalking测试.md
@@ -0,0 +1,22 @@
+## win11 本地测试
+架构图：
+
+https://skywalking.apache.org/zh/2020-04-19-skywalking-quick-start/0081Kckwly1gkl533oq0xj30ww0pomzt.jpg
+
+
+## 启动oap服务
+* oap接受上报的服务
+
+```shell
+
+docker pull apache/skywalking-oap-server:10.0.0
+
+```
+## 启动ui
+
+```shell
+apache/skywalking-ui
+
+```
+
+## agent使用
---
+++ swak-client/src/main/java/com/swak/lib/client/fegin/SwakFeignInterceptor.java
@@ -0,0 +1,25 @@
+package com.swak.lib.client.fegin;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+
+/**
+ * feign调用拦截器
+ * traceId
+ * header 头设置
+ * <p>
+ * 还有日志记录
+ *
+ * @author: ljq
+ * @date: 2024/11/6
+ */
+public class SwakFeignInterceptor implements RequestInterceptor {
+
+
+    @Override
+    public void apply(RequestTemplate template) {
+
+        
+
+    }
+}
